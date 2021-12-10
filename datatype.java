c69127202c099542a63be871dea82197cdd97d26
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-12-10 10:59:14 +0530

    commit

---
+++ datatype.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int num1=8;
+		float num2=8.1234f;
+		char ch='a';
+		boolean c=true;
+		 System.out.println(num1);
+		 System.out.println(num2);
+		 System.out.println(ch);
+		 System.out.println(c);
+		 
+	}
+
+}
