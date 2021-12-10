3074c5b055e1df4dfcb6108a549034be70a7546f
jayasankar-shyam <jayasankar06shyam@gmail.com>
2021-12-10 20:57:48 +0530

    test

--- sum.java
+++ sum.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class SumOfTwo {
 
 	public static void main(String[] args) {
-		
-		int x,y,s;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter 2 numbers : ");
-		x=sc.nextInt();
-		y =sc.nextInt();
-		s=x+y;
-		System.out.println("The Sum is "+s);
+
+		int x, y, s;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 number : ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		s = x + y;
+		System.out.println("The Sum is " + s);
 	}
 
 }
