7a4d09820d1d05b229c3b5f353d69575eb26fcf3
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2021-12-17 10:54:36 +0530

    Create frequency.java

---
+++ frequency.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Frequency {
+
+	public static void main(String[] args) {
+		int i,c=0,len;
+		char ch;
+		String word;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a word : ");
+		word=scan.next();
+		System.out.println("Enter a character to check : ");
+		ch=scan.next().charAt(0);
+		len=word.length();
+		for(i=0;i<len;i++)
+		{
+			if(word.charAt(i)==ch)
+				c++;
+		}
+		System.out.println(ch+" repeats for "+c+" times");
+	}
+}
