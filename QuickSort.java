0b9a27ff719f146dd5a3feea19d6b23f98f21cea
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2022-02-18 10:43:39 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,21 +1,19 @@
-package PROGRAM1;
+package hello;
 import java.util.Scanner;
-public class quicksort
+public class QuickSort
 {
     public static void main(String[] args)
     {
         int n;
         String temp;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter number of names you want to enter:");
-        n = s.nextInt();
-        String names[] = new String[n];
+        Scanner sc = new Scanner(System.in);
         Scanner s1 = new Scanner(System.in);
+        System.out.print("Enter number of names you want to enter:");
+        n = sc.nextInt();
+        String names[] = new String[n];
         System.out.println("Enter all the names:");
         for(int i = 0; i < n; i++)
-        {
             names[i] = s1.nextLine();
-        }
         for (int i = 0; i < n; i++)
         {
             for (int j = i + 1; j < n; j++)
@@ -30,9 +28,7 @@ public class quicksort
         }
         System.out.print("Names in Sorted Order:");
         for (int i = 0; i < n - 1; i++)
-        {
             System.out.print(names[i] + ",");
-        }
         System.out.print(names[n - 1]);
     }
-	}
+}
