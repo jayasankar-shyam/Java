53d7a293014214e1fec3376e06677c615073f899
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2022-02-18 11:10:35 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,56 +1,163 @@
 package hello;
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.*;
-public class TrafficLight  extends JFrame implements ActionListener {
-	JLabel label;
-	JTextField display;
-	JRadioButton r1,r2,r3;
-	ButtonGroup bg;
-	Container c;
-	TrafficLight(){
-		setLayout(new FlowLayout());
-		c=getContentPane();
-		label=new JLabel(" Traffic Light");
-		display =new JTextField(20);
-		r1=new JRadioButton("Red");
-		r2=new JRadioButton("Yellow");
-		r3=new JRadioButton("Green");
-		bg=new ButtonGroup();
-		c.add(label);
-		c.add(r1);
-		c.add(r2);
-		c.add(r3);
-		c.add(display);
-		bg.add(r1);
-		bg.add(r2);
-		bg.add(r3);
-		r1.addActionListener(this);
-		r2.addActionListener(this);
-		r3.addActionListener(this);
-		setSize(500,500);
-		setVisible(true);
-		c.setBackground(Color.white);
+ 
+ 
+public class Calculator implements ActionListener
+{
+	JFrame f;
+	JTextField t;
+	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bdiv,bmul,bsub,badd,beq,bdel,bclr;
+	static double a=0,b=0,result=0;
+	static int operator=0;
+	Calculator(){
+		f=new JFrame("Calculator");
+		t=new JTextField();
+		b1=new JButton("1");
+		b2=new JButton("2");
+		b3=new JButton("3");
+		b4=new JButton("4");
+		b5=new JButton("5");
+		b6=new JButton("6");
+		b7=new JButton("7");
+		b8=new JButton("8");
+		b9=new JButton("9");
+		b0=new JButton("0");
+		bdiv=new JButton("/");
+		bmul=new JButton("*");
+		bsub=new JButton("-");
+		badd=new JButton("+");
+		beq=new JButton("=");
+		bclr=new JButton("C");
+		t.setBounds(30,40,250,30);
+		b1.setBounds(40,100,50,40);
+		b2.setBounds(100,100,50,40);
+		b3.setBounds(160,100,50,40);
+		bdiv.setBounds(220,250,50,40);
+		b4.setBounds(40,150,50,40);
+		b5.setBounds(100,150,50,40);
+		b6.setBounds(160,150,50,40);
+		bmul.setBounds(220,200,50,40);
+		b7.setBounds(40,200,50,40);
+		b8.setBounds(100,200,50,40);
+		b9.setBounds(160,200,50,40);
+		bsub.setBounds(220,150,50,40);
+		b0.setBounds(100,250,50,40);
+		beq.setBounds(160,250,50,40);
+		badd.setBounds(220,100,50,40);
+		bclr.setBounds(40,250,50,40);
+		f.add(t);
+		f.add(b7);
+		f.add(b8);
+		f.add(b9);
+		f.add(bdiv);
+		f.add(b4);
+		f.add(b5);
+		f.add(b6);
+		f.add(bmul);
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(bsub);
+		f.add(b0);
+		f.add(beq);
+		f.add(badd);
+		f.add(bclr);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setSize(320,350);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		b0.addActionListener(this);
+		badd.addActionListener(this);
+		bdiv.addActionListener(this);
+		bmul.addActionListener(this);
+		bsub.addActionListener(this);
+		beq.addActionListener(this);
+		bclr.addActionListener(this);
 	}
-	public void actionPerformed(ActionEvent ie)
-	{
-		if(ie.getSource()==r1)
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource()==b1)
+		t.setText(t.getText().concat("1"));
+		if(e.getSource()==b2)
+		t.setText(t.getText().concat("2"));
+		if(e.getSource()==b3)
+		t.setText(t.getText().concat("3"));
+		if(e.getSource()==b4)
+		t.setText(t.getText().concat("4"));
+		if(e.getSource()==b5)
+		t.setText(t.getText().concat("5"));
+		if(e.getSource()==b6)
+		t.setText(t.getText().concat("6"));
+		if(e.getSource()==b7)
+		t.setText(t.getText().concat("7"));
+		if(e.getSource()==b8)
+		t.setText(t.getText().concat("8"));
+		if(e.getSource()==b9)
+		t.setText(t.getText().concat("9"));
+		if(e.getSource()==b0)
+		t.setText(t.getText().concat("0"));
+		if(e.getSource()==badd)
 		{
-			c.setBackground(Color.red);
-			display.setText("RED : TURNED ON");
+			a=Double.parseDouble(t.getText());
+			operator=1;
+			t.setText("");
 		}
-		else if (ie.getSource()==r2)
+		if(e.getSource()==bsub)
 		{
-			c.setBackground(Color.yellow);
-			display.setText("YELLOW : TURNED ON");
+			a=Double.parseDouble(t.getText());
+			operator=2;
+			t.setText("");
 		}
-		else if (ie.getSource()==r3)
+		if(e.getSource()==bmul)
 		{
-			c.setBackground(Color.green);
-			display.setText("GREEN : TURNED ON");
+			a=Double.parseDouble(t.getText());
+			operator=3;
+			t.setText("");
+		}
+		if(e.getSource()==bdiv)
+		{
+			a=Double.parseDouble(t.getText());
+			operator=4;
+			t.setText("");
+		}
+		if(e.getSource()==beq)
+		{
+			b=Double.parseDouble(t.getText());
+			switch(operator)
+			{
+				case 1: result=a+b;
+				break;
+				case 2: result=a-b;
+				break;
+				case 3: result=a*b;
+				break;
+				case 4: result=a/b;
+				break;
+				default: result=0;
+			}
+			t.setText(""+result);
+		}
+		if(e.getSource()==bclr)
+			t.setText("");
+		if(e.getSource()==bdel)
+		{
+			String s=t.getText();
+			t.setText("");
+			for(int i=0;i<s.length()-1;i++)
+			t.setText(t.getText()+s.charAt(i));
 		}
 	}
-	public static void main(String[] args) {
-	       new TrafficLight ();
-	}
+public static void main(String[] args) {
+			new Calculator();
+		}
 }
